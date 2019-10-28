18ab9eef70c43fde709c6ba4dd84d184c588ccae
haijun-suyan <15261882407@163.com>
2019-10-28 16:35:24 +0800

    测试文件

---
+++ ConvenienceKitHaris/Classes/TestViewController.h
@@ -0,0 +1,17 @@
+//
+//  TestViewController.h
+//  TestDemoXib
+//
+//  Created by haijunyan on 2019/10/28.
+//  Copyright © 2019 haijunyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ConvenienceKitHaris/Classes/TestViewController.m
@@ -0,0 +1,32 @@
+//
+//  TestViewController.m
+//  TestDemoXib
+//
+//  Created by haijunyan on 2019/10/28.
+//  Copyright © 2019 haijunyan. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
