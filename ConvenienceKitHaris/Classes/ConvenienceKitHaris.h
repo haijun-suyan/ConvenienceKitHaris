51dbb6f85945bde04e75f2c6a6001840f67a6cb1
yanhaijun <15261882407@163.com>
2023-09-26 17:12:21 +0800

    Type:ReqDev Desc:供外部业务层直接/间接访问 Version:

--- ConvenienceKitHaris/Classes/ConvenienceKitHaris.h
+++ ConvenienceKitHaris/Classes/ConvenienceKitHaris.h
@@ -7,6 +7,12 @@
 
 #ifndef ConvenienceKitHaris_h
 #define ConvenienceKitHaris_h
-
+//实现供业务项目层的间接(具体)访问
+#import "UIColor+Additions.h"
+#import "HarisUtil.h"
+#import "UIView+Responder.h"
+#import "ResponderChainViewController.h"
+#import "NSObject+TimerDown.h"
+#import "NSString+Attributed.h"
 
 #endif /* ConvenienceKitHaris_h */
