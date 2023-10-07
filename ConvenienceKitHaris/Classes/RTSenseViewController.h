bdd82f5935876619aefb5a6f6e5421903cf616d1
yanhaijun <15261882407@163.com>
2023-10-07 17:27:07 +0800

    Type:ReqDev Desc:RunTime框架的API使用场景 Version:

---
+++ ConvenienceKitHaris/Classes/RTSenseViewController.h
@@ -0,0 +1,12 @@
+//
+//  RTSenseViewController.h
+//  ConvenienceKitHaris
+//  
+//  Created by haijunyan on 2023/10/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTSenseViewController : UIViewController
+
+@end
---
+++ ConvenienceKitHaris/Classes/RTSenseViewController.m
@@ -0,0 +1,21 @@
+//
+//  RTSenseViewController.m
+//  ConvenienceKitHaris
+//  RunTime框架的API使用场景
+//  Created by haijunyan on 2023/10/7.
+//
+
+#import "RTSenseViewController.h"
+
+@interface RTSenseViewController ()
+
+@end
+
+@implementation RTSenseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
