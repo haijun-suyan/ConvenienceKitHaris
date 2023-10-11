fb1afd6569546461d360d539b400d9c33b821684
yanhaijun <15261882407@163.com>
2023-10-11 20:07:33 +0800

    Type:ReqDev Desc:二方库维护 Version:

--- ConvenienceKitHaris/Classes/KVCUseRuleViewController.m
+++ ConvenienceKitHaris/Classes/KVCUseRuleViewController.m
@@ -15,7 +15,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
 }
 
 
--- ConvenienceKitHaris/Classes/Product.h
+++ ConvenienceKitHaris/Classes/Product.h
@@ -8,5 +8,8 @@
 #import <Foundation/Foundation.h>
 
 @interface Product : NSObject
+@property (nonatomic, copy) NSString *name;    //商品名字
+@property (nonatomic, assign) float price;     //商品价格
+//@property (nonatomic, strong) Factory *factory; //生产工厂
 
 @end
